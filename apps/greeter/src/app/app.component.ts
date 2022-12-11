b8f5553bf3377a4bea5402083c6fc5480e3cfee1
David Scourfield <daiscog@users.noreply.github.com>
2022-12-11 13:29:48 +0000

    Initial commit

---
+++ apps/greeter/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cypress-testid-proxy-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'greeter';
+}
